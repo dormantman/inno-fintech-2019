ad087abd798e84902793126dd3cb965f3f1bc62e
dormantman <dormantman@ya.ru>
2019-03-05 01:09:53 +0300

    Added: miner function

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 block_chain_path="./private-chain-data/"
 network_id=228
+etherbase="0xa85968c7ca4ac0b0bcd91b6bf4327d3336b1571c"
 
 geth --datadir ${block_chain_path} --rpc --rpcaddr "0.0.0.0" \
-    --rpcapi "rpc,personal,eth,net,web3" --networkid ${network_id} console
+    --rpcapi "rpc,personal,eth,net,web3" --networkid ${network_id} console --mine --minerthreads=1 --etherbase=${etherbase}
