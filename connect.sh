7279bdedfc8533aeccbcd675fb49580e4e925745
dormantman <dormantman@ya.ru>
2019-03-05 01:35:36 +0300

    Some fix

--- connect.sh
+++ connect.sh
@@ -2,6 +2,7 @@
 
 network_id=228
 node_address="enode://fee640f78984f9c91caae254ede59e2cba4cbfa35c201e43730c8d95d08764ee30c4c0a743e84326001565dd025159d14b8572ff906a5a0d9626102a059677dd@10.178.232.53:30303"
+etherbase="0xa85968c7ca4ac0b0bcd91b6bf4327d3336b1571c"
 
 geth --rpc --rpcaddr "0.0.0.0" --rpcapi "rpc,personal,eth,net,web3" --networkid ${network_id} \
-    --bootnodes ${node_address} console
+    --bootnodes ${node_address} console --etherbase=${etherbase}
--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ network_id=228
 etherbase="0xa85968c7ca4ac0b0bcd91b6bf4327d3336b1571c"
 
 geth --datadir ${block_chain_path} --rpc --rpcaddr "0.0.0.0" \
-    --rpcapi "rpc,personal,eth,net,web3" --networkid ${network_id} console --mine --minerthreads=1 --etherbase=${etherbase}
+    --rpcapi "rpc,personal,eth,net,web3" --networkid ${network_id} console --etherbase=${etherbase}
